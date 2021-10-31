2d33469a4b476bbc3761ba36ec02413d8629bc6a
JackenQuake <jackenquake@gmail.com>
2021-11-01 02:46:58 +0300

    Task2 done

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -10,6 +10,34 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("FizzBuzz test programm.");
+            Console.Write("Type max number (Default = 100): ");
+            var MXN = Console.ReadLine();
+            int MaxNumber;
+
+            try {
+                MaxNumber = Int32.Parse(MXN);
+            }
+            catch (FormatException Error) { MaxNumber = 100; }
+            
+            if (MaxNumber <= 0) MaxNumber = 100;
+
+            Console.WriteLine($"Lets try UP to {MaxNumber}");
+
+            for (int i = 1; i <= MaxNumber; i++) {
+                if (i % 3 == 0) {
+                    if (i % 5 == 0) Console.WriteLine("fizz buzz");
+                    else Console.WriteLine("fizz");
+                }
+                else {
+                    if (i % 5 == 0) Console.WriteLine("buzz");
+                    else Console.WriteLine($"{i}");
+                }
+            }
+
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+
         }
     }
 }
