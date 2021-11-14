78a744381674faa118c6ef41f6fc0719a91913db
JackenQuake <jackenquake@gmail.com>
2021-11-14 04:28:58 +0300

    Task1 done

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter any string to write to file: ");
+            var str = Console.ReadLine();
+            string myFile = "L5T1.txt";
+            File.WriteAllText(myFile, str);
+            Console.WriteLine(); Console.Write("Press any key to exit..."); Console.ReadKey();
+        }
+    }
+}
